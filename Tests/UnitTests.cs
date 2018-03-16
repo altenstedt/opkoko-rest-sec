5c20899a72e64a0f8e78fc7b906889ad1c63a007
Martin Altenstedt <Martin.Altenstedt@gmail.com>
2018-03-16 12:48:23 +0100

    Add unit test for successful resource access
    Testing successful resource access is important to verify that the
    correct claim is needed to authorize access.  If we did not, then
    requiring a lower claim, e.g. "read:guest" would not be caught by the
    403 test.  This commit will add a test that verifies the 200 response.

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -16,7 +16,7 @@ namespace Tests
         {
             var controller = new ProductsController();
 
-            var claims = new[] { new Claim("urn:my:claim", "42") };
+            var claims = new[] { new Claim("scope", "not a valid scope") };
             var identity = new ClaimsIdentity(claims);
             var principal = new ClaimsPrincipal(identity);
 
@@ -28,6 +28,29 @@ namespace Tests
             var result = controller.Get();
 
             Assert.IsType<ForbidResult>(result);
+        }
+
+        // Testing successful resource access is important to verify that the
+        // correct claim is needed to authorize access.  If we did not, then
+        // requiring a lower claim, e.g. "read:guest" would not be caught by the
+        // 403 test above; This test will catch such configuration errors.
+        [Fact]
+        public void GetProductsShouldReturn200WhenAuthorized()
+        {
+            var controller = new ProductsController();
+
+            var claims = new[] { new Claim("scope", "read:product") };
+            var identity = new ClaimsIdentity(claims);
+            var principal = new ClaimsPrincipal(identity);
+
+            controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = principal }
+            };
+
+            var result = controller.Get();
+
+            Assert.IsType<OkObjectResult>(result);
         }
     }
 }
