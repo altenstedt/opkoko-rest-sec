67b87a4e61feb779e01b546e9ec7090d5a3cc0a9
Martin Altenstedt <Martin.Altenstedt@gmail.com>
2018-03-16 13:12:06 +0100

    Comment out unit test implementation
    This commit will remove the correct implementation of the unit tests,
    and adds some template code that the student can use for inspiration
    in writing her own tests.
    
    This commit represents where the student should start the excersice.

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -14,43 +14,30 @@ namespace Tests
         [Fact]
         public void GetProductsShouldReturn403WhenMissingClaims()
         {
-            var controller = new ProductsController();
-
-            var claims = new[] { new Claim("scope", "not a valid scope") };
-            var identity = new ClaimsIdentity(claims);
-            var principal = new ClaimsPrincipal(identity);
-
-            controller.ControllerContext = new ControllerContext
-            {
-                HttpContext = new DefaultHttpContext { User = principal }
-            };
-
-            var result = controller.Get();
-
-            Assert.IsType<ForbidResult>(result);
+            throw new NotImplementedException();
         }
 
-        // Testing successful resource access is important to verify that the
-        // correct claim is needed to authorize access.  If we did not, then
-        // requiring a lower claim, e.g. "read:guest" would not be caught by the
-        // 403 test above; This test will catch such configuration errors.
         [Fact]
         public void GetProductsShouldReturn200WhenAuthorized()
         {
-            var controller = new ProductsController();
-
-            var claims = new[] { new Claim("scope", "read:product") };
-            var identity = new ClaimsIdentity(claims);
-            var principal = new ClaimsPrincipal(identity);
-
-            controller.ControllerContext = new ControllerContext
-            {
-                HttpContext = new DefaultHttpContext { User = principal }
-            };
-
-            var result = controller.Get();
-
-            Assert.IsType<OkObjectResult>(result);
+            throw new NotImplementedException();
         }
     }
 }
+
+/*
+    var controller = new ProductsController();
+    
+    var claims = new Claim[] { };
+    var identity = new ClaimsIdentity(claims);
+    var principal = new ClaimsPrincipal(identity);
+    
+    controller.ControllerContext = new ControllerContext
+    {
+        HttpContext = new DefaultHttpContext { User = principal }
+    };
+    
+    var result = controller.Get();
+    
+    Assert.IsType<UnauthorizedResult>(result);
+ */
